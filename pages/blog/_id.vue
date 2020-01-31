6c60c1cac20e7677d1192f7ea09d548bfb6e8656
Óscar Eusse <scr.eusse@gmail.com>
2020-01-30 19:08:52 -0500

    [Add] Uso del metodo asyncData de nuxt para poder manejar la obtencion de datos asincronos

--- pages/blog/_id.vue
+++ pages/blog/_id.vue
@@ -1,12 +1,45 @@
 <template>
     <div class="container">
-        <h1>Detalle del artículo id: {{ $route.params.id }}</h1>
+        <div class="card">
+            <div class="card-body">
+                <h1>{{ post.title }}</h1>
+                <p>{{ post.body }}</p>
+                <p class="small">{{ post.author }}</p>
+                <nuxt-link v-if="post.title" to="/blog" class="btn btn-primary">Atras</nuxt-link>
+            </div>
+        </div>
     </div>
 </template>
 <script>
+import axios from 'axios'
+
 export default {
     head: {
         title: `Blog - Artículo`
     },
+    data() {
+        return {}
+    },
+    async asyncData({isDev, route, store, env, params, query, req, res, redirect, error}) {
+        try {
+            let post = {}
+            let author = {}
+            
+            const postResponse = await axios.get(`https://jsonplaceholder.typicode.com/posts/${params.id}`)
+            post = postResponse.data
+
+            const userId = postResponse.data.userId
+
+            const authorResponse = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`)
+            author = authorResponse.data
+
+            post.author = author.name
+
+            return { post, author }
+        } catch (error) {
+            console.error(error)
+            return { error }
+        }
+    }
 }
 </script>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,18 +1,20 @@
 <template>
     <div class="container mt-5">
         <div class="row">
-            <h1>Blog</h1>
-        </div>
-        <div class="row">
-            <div class="col-md-3" v-for="(article, index) in articles" :key="index">
-                <h2>
-                    <nuxt-link :to="`/blog/${article.id}`">{{ article.text }}</nuxt-link>
-                </h2>                
+            <div class="card my-2" v-for="post in posts" :key="post.id">
+                <div class="card-body">
+                    <nuxt-link :to="`/blog/${post.id}`">
+                        <h3>{{ post.title }}</h3>
+                    </nuxt-link>
+                    <p>{{ post.body }}</p>
+                </div>
             </div>
         </div>
     </div>
 </template>
 <script>
+import axios from 'axios'
+
 export default {
     name: 'blog',
     head: {
@@ -20,12 +22,22 @@ export default {
     },
     data() {
         return {
-            articles: [
-                { id: 1, text: 'Titulo #1' },
-                { id: 2, text: 'Titulo #2' },
-                { id: 3, text: 'Titulo #3' },
-                { id: 4, text: 'Titulo #4' },
-            ]
+            posts: []
+        }
+    },
+    created() {
+        this.getPosts()
+    },
+    methods: {
+        async getPosts() {
+            try {
+                const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+                if (response.status === 200) {
+                    this.posts = response.data
+                }
+            } catch (error) {
+                console.error(error)
+            }
         }
     }
 }
